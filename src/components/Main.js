543fd1acb62ac38dd2965c51b325f6634b031ad3
Alan <a386226@gmail.com>
2018-11-15 15:46:19 -0800

    adjusted image sizes

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,9 +7,11 @@ class Main extends Component {
   render() {
     return (
       <div className="main">
-        <div id="image">
+        <div id="imgDiv">
+          <div id="image">
           <img src={waiting} className="waiting" alt="waiting backstage" />
           <p className="attribute">Waiting Backstage by Todd Trotta</p>
+          </div>
         </div>
       </div>
     );
