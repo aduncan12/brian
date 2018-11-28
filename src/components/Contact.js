c0e4a423c2c92b556f3606cc07fa9944b7aa4a14
Alan <a386226@gmail.com>
2018-11-28 10:40:48 -0800

    contact page done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,14 +11,14 @@ class Contact extends Component {
           <p className="attribute">Lamplighter by Todd Trotta</p>
         </div>
         
-        {/* <div id="contactForm">
+        <div id="contactForm">
           <form className="message" method="GET" action="/page">   
             <input type="text" className="comment" id="commentName" name="name" placeholder="Name" />
             <input type="text" className="comment" id="commentEmail" name="email" placeholder="Email" />
             <textarea type="text" className="comment" id="commentComment" name="comment" placeholder="Say hi..." />                  
             <input type="submit" className="comment" id="commentSubmit" value="SUBMIT" />
           </form>
-        </div>  */}
+        </div> 
       </div>
     );
   }
